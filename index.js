afe8d9d2c4ef269d7749588d574fccf27b4791eb
Jason Engel <engel.jason@gmail.com>
2021-10-01 21:19:34 -0700

    start refactor to d3 page

---
+++ index.js
@@ -0,0 +1,7 @@
+var body = d3.select('body')
+body.text("Hello World")
+
+var navbar = d3.create('nav').class('navbar', 'navbar-default')
+                .append('div').class('container-fluid', 'navbar-custom')
+                .append('div').class('collapse', 'navbar-collapse').id('bs-example-navbar-collapse-1')
+                .
